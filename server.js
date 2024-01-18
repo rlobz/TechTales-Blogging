1f9266f49aad21bafbcde970d9d53d036bb6cc2a
rlobz <rlobzowski@gmail.com>
2024-01-18 20:11:54 +0000

    another try

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require("./controllers/"));
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+  sequelize.sync({ force: false });
 });
